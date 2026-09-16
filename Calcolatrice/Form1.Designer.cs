235028acf091a427cd4c2387d34d2dc6c395e28c
unknown <d.racca.4118@vallauri.net>
2026-09-16 11:15:58 +0200

    Creazione dinamica bottoni semplificati

--- Calcolatrice/Form1.Designer.cs
+++ Calcolatrice/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(300, 492);
+            this.ClientSize = new System.Drawing.Size(322, 477);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Form1";
--- Calcolatrice/Form1.cs
+++ Calcolatrice/Form1.cs
@@ -12,6 +12,16 @@ namespace Calcolatrice
 {
     public partial class Form1 : Form
     {
+        private char[,] buttons =
+        {
+            {'%','\u0152','C','\u232B'},
+            {'\u215F','\u00B2','\u221A','\u00F7'},
+            {'7','8','9','x'},
+            {'4','5','6','-'},
+            {'1','2','3','+'},
+            {'\u00B1','0',',','='},
+
+        };
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +29,30 @@ namespace Calcolatrice
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MakeButtons();
+        }
 
+        private void MakeButtons()
+        {
+            int btnWidth = 80,btnHeight = 60;
+            int posY = 106;
+            for (int i = 0; i < buttons.GetLength(0); i++)
+            {
+                int posx = 0;
+                for (int j = 0; j < buttons.GetLength(1); j++)
+                {
+                    Button btn = new Button();
+                    btn.Width=btnWidth;
+                    btn.Height=btnHeight;
+                    btn.Top=posY;
+                    btn.Left=posx;
+                    btn.Font = new Font("Segoe UI",16);
+                    btn.Text=buttons[i,j].ToString();
+                    Controls.Add(btn);
+                    posx+= btnWidth;
+                }
+                posY += btnHeight;
+            }
         }
     }
 }
