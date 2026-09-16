8dbbec244b012859c08ec469536b8dbb7afd405f
unknown <d.racca.4118@vallauri.net>
2026-09-16 11:38:01 +0200

    CREAZIONE DINAMICA BOTTONI

--- Calcolatrice/Form1.cs
+++ Calcolatrice/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,19 +13,39 @@ namespace Calcolatrice
 {
     public partial class Form1 : Form
     {
-        private char[,] buttons =
+        static private Color OPERATION_BG = Color.LightGray;
+        static private Color NUMBER_BG = Color.WhiteSmoke;
+        static private Color EQUAL_BG = Color.LightSeaGreen;
+
+        public struct btnStruct 
         {
-            {'%','\u0152','C','\u232B'},
-            {'\u215F','\u00B2','\u221A','\u00F7'},
-            {'7','8','9','x'},
-            {'4','5','6','-'},
-            {'1','2','3','+'},
-            {'\u00B1','0',',','='},
+            public char Content;
+            public Color BgColor;
+            public btnStruct(char content,Color Bgcolor)
+            {
+                this.Content = content;
+                this.BgColor = Bgcolor;
+            }
+        }
+        private btnStruct[,] buttons =
+        {
+
+
+           {new btnStruct('%',OPERATION_BG),new btnStruct('\u0152',OPERATION_BG),new btnStruct('C',OPERATION_BG),new btnStruct('\u232B',OPERATION_BG)},
+           {new btnStruct('\u215F', OPERATION_BG),new btnStruct('\u00B2', OPERATION_BG),new btnStruct('\u221A', OPERATION_BG),new btnStruct('\u00F7', OPERATION_BG)      },
+           {new btnStruct('7', NUMBER_BG),new btnStruct('8', NUMBER_BG),new btnStruct('9', NUMBER_BG),new btnStruct('x', OPERATION_BG)},
+           {new btnStruct('4', NUMBER_BG),new btnStruct('5', NUMBER_BG),new btnStruct('6', NUMBER_BG),  new btnStruct('-', OPERATION_BG)    },
+           {new btnStruct('1', NUMBER_BG),new btnStruct('2', NUMBER_BG),new btnStruct('3', NUMBER_BG),new btnStruct('+', OPERATION_BG)},
+           {new btnStruct('\u00B1', NUMBER_BG),new btnStruct('0', NUMBER_BG),new btnStruct(',', NUMBER_BG),new btnStruct('=', EQUAL_BG)},
+
 
         };
+        
         public Form1()
         {
             InitializeComponent();
+            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -47,7 +68,8 @@ namespace Calcolatrice
                     btn.Top=posY;
                     btn.Left=posx;
                     btn.Font = new Font("Segoe UI",16);
-                    btn.Text=buttons[i,j].ToString();
+                    btn.Text=buttons[i,j].Content.ToString();
+                    btn.BackColor = buttons[i, j].BgColor;
                     Controls.Add(btn);
                     posx+= btnWidth;
                 }
